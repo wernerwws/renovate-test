d5ee2cd9df20298978a7fbae952255f61a899b75
Werner Schmid <schmid@redlever.solutions>
2023-05-31 16:44:23 +0200

    Add Renovate config.

---
+++ renovate/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "dependencyDashboard": true,
+    enabledManagers: [
+        "dockerfile",
+        "helmv3",
+        "helm-values",
+        "regex",
+        "pre-commit",
+        "terraform",
+        "terraform-version",
+        "terragrunt",
+        "terragrunt-version",
+        "gitlabci",
+        "gomod",
+    ],
+}
