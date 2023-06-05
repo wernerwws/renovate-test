5725ad4f1395597a2f0014b9680380d8adcf7e16
Werner Schmid <schmid@redlever.solutions>
2023-06-05 11:51:40 +0200

    test.

--- renovate/config.js
+++ renovate/config.js
@@ -18,7 +18,7 @@ module.exports = {
             {
                 "fileMatch": ["test.txt"],
                 "matchStrings": [
-                    "(.*image|.*IMAGE)[^a-zA-Z]*(?<registryUrl>.*?)\\/(?<depName>.*?):(?<currentValue>.*?)\\s"
+                    "(.*image|.*IMAGE)[^a-zA-Z]*(?<registryUrl>.*?)\\/(?<depName>.*?):(?<currentValue>.*?)@?(?<currentDigest>sha256:[a-f0-9]+)?\\s",
                 ],
                 "datasourceTemplate": "docker"
             },
